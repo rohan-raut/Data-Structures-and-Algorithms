cf46a319015faaf3becd1326ae84c62814a7207a
Rohan <rohanraut124@gmail.com>
2022-09-10 21:06:29 +0530

    Updated

---
+++ trees/binary_lifting.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Tree is rooted at 0
+
+class binaryLifting{
+    public:
+    vector<vector<int>> ancestor;
+    int N;
+
+    binaryLifting(int n, vector<vector<int>> &tree){
+        N = n;
+        vector<int> parent(n,0);
+        for(int i=0; i<n; i++){
+            
+        }
+    }
+
+    binaryLifting(int n, vector<int> &parent){
+
+    }
+
+};
+
+
+int main()
+{
+    
+    
+    return 0;
+}
