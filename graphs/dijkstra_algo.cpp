8faac1eb0eb70296c7bff8fe5e3b3a6f666a83fd
Rohan <rohanraut124@gmail.com>
2023-02-09 11:59:31 +0530

    Updated Shortest Path Algorithms

--- graphs/dijkstra_algo.cpp
+++ graphs/dijkstra_algo.cpp
@@ -1,65 +1,36 @@
-/*
-*   author: rohanraut
-*   created on: 28.08.2021 12:24:46
-*/
-
-// Reference Link:
-// 1. https://www.youtube.com/watch?v=smHnz2RHJBY&list=PLm77mruelczpPDzLgp4UefbQRT4-cyJsW&index=12
-// 2. https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-greedy-algo-7/
-
-// First do Prim's algorithm and its implemention
-
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int V=9;
+vector<int> dijkstras(vector<vector<pair<int,int>>> &graph, int src){
+    int n = graph.size();
+    vector<int> dist(n+1, 1e7);
+    vector<int> par(n+1, -1); // to trase the path
+    priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>>> pq;
 
-int minKey(vector<int> &key, vector<bool> &mstSet){
-    int min=INT_MAX, min_index;
-    for(int i=0; i<V; i++){
-        if(mstSet[i]==false && key[i]<=min){
-            min=key[i];
-            min_index=i;
-        }
-    }
-    return min_index;
-}
+    dist[src] = 0;
+    pq.push({0, src});
 
-void printpath(vector<int> &parent, vector<int> &key){
-    cout<<"Path:\t Distance\n";
-    for(int i=0; i<V; i++){
-        cout<<i<<"\t"<<key[i]<<endl;
-    }
-}
+    while(!pq.empty()){
+        int d = pq.top().first;
+        int node = pq.top().second;
+        pq.pop();
 
-void dijkstra(vector<vector<int>> &graph, int src){
-    vector<int> parent(V);
-    vector<bool> mstSet(V, false);
-    vector<int> key(V,INT_MAX);
-
-    parent[0]=-1;
-    key[0]=0;
-
-    for(int i=0; i<V; i++){
-        int u= minKey(key, mstSet);
-        mstSet[u]=true;
-
-        for(int v=0; v<V; v++){
-            if(mstSet[v]==false && graph[u][v]!=0 && (graph[u][v]+key[u])<key[v]){
-                parent[v]=u;
-                key[v]=key[u]+graph[u][v];
+        for(int i=0; i<graph[node].size(); i++){
+            if(dist[node] + graph[node][i].second < dist[graph[node][i].second]){
+                dist[graph[node][i].second] = dist[node] + graph[node][i].second;
+                pq.push({(dist[node] + graph[node][i].second), graph[node][i].second});
+                par[graph[node][i].second] = node;
             }
         }
     }
 
-    printpath(parent, key);
+    return dist;
 }
+
 
 int main()
 {
-    vector<vector<int>> graph = {{0, 4, 0, 0, 0, 0, 0, 8, 0}, {4, 0, 8, 0, 0, 0, 0, 11, 0}, {0, 8, 0, 7, 0, 4, 0, 0, 2}, {0, 0, 7, 0, 9, 14, 0, 0, 0}, {0, 0, 0, 9, 0, 10, 0, 0, 0}, {0, 0, 4, 14, 10, 0, 2, 0, 0}, {0, 0, 0, 0, 0, 2, 0, 1, 6}, {8, 11, 0, 0, 0, 0, 1, 0, 7}, {0, 0, 2, 0, 0, 0, 6, 7, 0}};
 
-    dijkstra(graph, 0);
     return 0;
 }
--- graphs/floyd_warshall_algorithm.cpp
+++ graphs/floyd_warshall_algorithm.cpp
@@ -1,54 +1,25 @@
-/*
-*   author: rohanraut
-*   created on: 28.08.2021 20:53:52
-*/
-
-// Reference Link:
-// 1. youtube.com/watch?v=Gc4mWrmJBsw
-// 2. https://www.geeksforgeeks.org/floyd-warshall-algorithm-dp-16/
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int V=4;
+// The graph should be represented using adjancency matrix
 
-void printGraph(vector<vector<int>> &graph){
-    for(int i=0; i<V; i++){
-        for(int j=0; j<V; j++){
-            cout<<graph[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-}
+vector<vector<int>> floyd(vector<vector<int>> &graph){
+    int n = graph.size();
+    vector<vector<int>> dist(n+1, vector<int> (n+1, 1e7));
 
-void algo(vector<vector<int>> graph){
-    for(int k=0; k<V; k++){
-        for(int i=0; i<V; i++){
-            for(int j=0; j<V; j++){
-                if(i==j){
-                    graph[i][j]=0;
-                }
-                if(i==k || j==k){
-                    continue;
-                }
-                if(((graph[i][k]+graph[k][j])<graph[i][j]) && graph[i][k]!=INT_MAX && graph[k][j]!=INT_MAX){
-                    graph[i][j]= (graph[i][k]+graph[k][j]);
-                }
+    for(int k=0; k<n; k++){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
     }
 
-    printGraph(graph);
+    return dist;
 }
 
 int main()
 {
-    vector<vector<int>> graph = {{0, 5, INT_MAX, 10}, 
-                                {INT_MAX, 0, 3, INT_MAX}, 
-                                {INT_MAX, INT_MAX, 0, 1}, 
-                                {INT_MAX, INT_MAX, INT_MAX, 0}};
 
-    algo(graph);
     return 0;
 }
